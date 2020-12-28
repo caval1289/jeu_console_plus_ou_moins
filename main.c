ed9fe7b6048ba07fa1d3857d3a67cf024f18ee53
cavaleri <caval1289@gmail.com>
2020-12-28 09:35:18 +0100

    avec function

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+int menuDeDifficulter();
+int menuSoloOuMulti();
+
 int main()
 {
     int nombreMystere = 0, nombreATrouve = 1,continuerPartie = 1;
@@ -12,72 +15,41 @@ int main()
     while (continuerPartie)
     {
 
-        int compt = 1, choixDuJoueur = 0, niveauDeDifficulte = 0, erreurMenuDifficulte = 1, erreurMenuModeDeJeux = 1;
+        int compt = 1;
 
 
-        while( erreurMenuDifficulte)
+        switch(menuDeDifficulter())
         {
-            printf("Choisis la difficulte\n");
-            printf("===Menu===\n");
-            printf("1. facile\n");
-            printf("2. intermediaire\n");
-            printf("3. Difficile\n");
-            printf("Ecrivez le chiffre qui correspond au niveau\n");
-            scanf("%d", &niveauDeDifficulte);
-
-            switch(niveauDeDifficulte)
-            {
-            case 1:
-                erreurMenuDifficulte = 0;
-                printf("Tu as choisi le mode facile tu dois trouver un nombre entre 1 et 100\n");
-                break;
-            case 2:
-                erreurMenuDifficulte = 0;
-                printf("Tu as choisi le mode intermediaire tu dois trouver un nombre entre 1 et 1000\n");
-                MAX = 1000;
-                break;
-            case 3:
-                erreurMenuDifficulte = 0;
-                printf("Tu as choisi le mode difficile tu dois trouver un nombre entre 1 et 10000\n");
-                MAX = 10000;
-            default:
-                printf("Le choix n'est pas bon merci de recommencer\n");
-                break;
-            }
+        case 1:
+            printf("Tu as choisi le mode facile tu dois trouver un nombre entre 1 et 100\n");
+            break;
+        case 2:
+            printf("Tu as choisi le mode intermediaire tu dois trouver un nombre entre 1 et 1000\n");
+            MAX = 1000;
+            break;
+        case 3:
+            printf("Tu as choisi le mode difficile tu dois trouver un nombre entre 1 et 10000\n");
+            MAX = 10000;
         }
 
-        while(erreurMenuModeDeJeux)
-        {
 
-
-            printf("Choisie ton mode de jeux\n");
-            printf("===Menu===\n");
-            printf("1. En solo\n");
-            printf("2. Multijoueur\n");
-            printf("Tape un ou deux\n");
-            scanf("%d", &choixDuJoueur);
-
-            switch(choixDuJoueur)
+            switch(menuSoloOuMulti())
             {
             case 1:
-                erreurMenuModeDeJeux =0;
                 printf("Tu as choisi le mode Solo\n");
                 srand(time(NULL));
                 nombreMystere =(rand() % (MAX - MIN +1)) + MIN;
                 break;
             case 2:
-                erreurMenuModeDeJeux =0;
                 printf("Tu as choisi le mode MultiJoueur\n");
                 printf("Choisi un nombre et ton partenaire doit le trouvé\n");
                 scanf("%d",&nombreMystere);
                 break;
-            default:
-                printf("Le choix n'est pas bon merci de recommencer\n");
-                break;
-            }
-        }
 
-        printf("Entre un nombre au clavier pour trouvé le bon chiffre\n");
+            }
+
+
+        printf("Entre un nombre au clavier pour trouvé le bon chiffre, tu dois trouver un nombre entre %d et %d\n", MIN, MAX);
         scanf("%d",&nombreATrouve);
 
         while(nombreATrouve != nombreMystere )
@@ -109,7 +81,7 @@ int main()
         }
 
         printf("Victoire vous l'avez trouver en %d coups\n",compt);
-        printf("Voulez-vous recommencer? si oui appuyez1 sur 1 sinon 0\n");
+        printf("Voulez-vous recommencer? si oui appuyez sur 1 sinon 0\n");
         scanf("%d", &continuerPartie);
 
         if (continuerPartie)
@@ -123,4 +95,48 @@ int main()
     }
 
     return 0;
+}
+
+
+int menuDeDifficulter ()
+{
+    int choix = 0;
+
+    while( choix < 1 || choix > 3)
+    {
+        printf("Choisis la difficulte\n");
+        printf("===Menu===\n");
+        printf("1. facile\n");
+        printf("2. intermediaire\n");
+        printf("3. Difficile\n");
+        printf("Ecrivez le chiffre qui correspond au niveau\n");
+        scanf("%d", &choix);
+        if (choix < 1 || choix >3)
+        {
+            printf("Le choix n'est pas bon merci de recommencer\n");
+        }
+    }
+
+    return choix;
+}
+
+int menuSoloOuMulti ()
+{
+  int choix = 0;
+
+         while(choix < 1 || choix > 2)
+        {
+            printf("Choisie ton mode de jeux\n");
+            printf("===Menu===\n");
+            printf("1. En solo\n");
+            printf("2. Multijoueur\n");
+            printf("Tape un ou deux\n");
+            scanf("%d", &choix);
+
+         if (choix<1 || choix > 2)
+         {
+             printf("Le choix n'est pas bon merci de recommencer\n");
+         }
+        }
+        return choix;
 }
